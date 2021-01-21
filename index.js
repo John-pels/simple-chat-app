8555317d316fd8af4b98aaad625b1a6af0035828
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2021-01-21 05:30:45 +0100

    I add user name

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const app = require("express")();
 const http = require("http").Server(app);
-const socket = require("socket.io")(http);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-socket.on("connection", (socket) => {
+users = [];
+io.on("connection", (socket) => {
   console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("setUsername", (data) => {
+    console.log(data);
+    if (users.indexOf(data) > -1) {
+      socket.emit(
+        "userExists",
+        data + " username is taken! Try some other username."
+      );
+    } else {
+      users.push(data);
+      socket.emit("userSet", { username: data });
+    }
+  });
+
+  socket.on("chat message", (data) => {
+    console.log("User message and username: " + data);
+    io.emit("chat message", data);
+  });
 });
 
 http.listen(3000, () => {
