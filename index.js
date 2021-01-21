c4ba95a8198e8ff71479e28b1e78b1428dee8280
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2021-01-21 03:51:53 +0100

    I update the file

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const app = require("express")();
-const http = require("http").createServer(app);
+const http = require("http").Server(app);
+const socket = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+socket.on("connection", (socket) => {
+  console.log("A user connected");
 });
 
 http.listen(3000, () => {
