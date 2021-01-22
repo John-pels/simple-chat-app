e52d5ec889f539c2108380411108b40c0be729a9
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2021-01-22 09:32:02 +0100

    I add cors wildcard

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, { cors: { origin: "*" } });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
